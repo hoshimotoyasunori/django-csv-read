2424fa9ce23ba6745d33c91757e9b6e21361c43c
hoshimotoyasunori <hoshimotoyasunori@gmail.com>
2023-01-13 22:50:02 +0900

    df_1にてデータの順番入れ替え

--- test/test.py
+++ test/test.py
@@ -1,16 +1,26 @@
 import pandas as pd
 # from pathlib import Path
 
-filepath = 'csv_input/sample.csv'
+filepath = 'csv_input/sample1.csv'
 # print(Path(filepath).read_text())
 
-# header=1 は1行目から読み込み開始
 # usecols=[1, 3, 6, 7] は読み込む行の指定[1.項目名 3.桁数 6.物理名 7.型]
-# index_col=0 行ラベルとなる列の指定
+# header=0 は0行目から読み込み開始
+# names=["item_name","digit","physical_name","type"]はheaderのラベル名指示
 
 
-df = pd.read_csv(filepath,header=1,usecols=[1, 3, 6, 7],index_col=0)
-print(df)
+df = pd.read_csv(
+        filepath,
+        usecols=[1, 3, 6, 7],
+        header=0,
+        names=["item_name","digit","physical_name","type"]
+        )
+
+# df_1=df.reindex(columns=["physical_name","type","item_name","digit"])はカラムの順番変更
+df_1=df.reindex(columns=["physical_name","type","item_name","digit"])
+
+print(df_1)
+
 
 # 出力
-df.to_csv("csv_output/out2.csv")
+df_1.to_csv("csv_output/out1.csv")
